7ca07665b72a96dd92acf82f520193c640804a07
enzoquarelo <enzo.quarelo@gmail.com>
2024-08-02 08:13:11 -0300

    exercício 2 - validar email

---
+++ xUnit_TestesUnitários/Email.Test/EmailTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Email.Test
+{
+    public class EmailTest
+    {
+        [Fact]
+        public void TestValidateEmail()
+        {
+            var email = "enzo.quarelo@gmail.com";
+
+            var result = Email.ValidateEmail(email);
+
+            Assert.Equal(email, result);
+
+        }
+
+        [Fact]
+        public void TestInvalidateEmail()
+        {
+            var email = "slasla";
+            var result = Email.ValidateEmail(email);
+            Assert.Equal("Não Válido", result);
+        }
+    }
+}
---
+++ xUnit_TestesUnitários/Email/Email.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Email
+{
+    public class Email
+    {
+        public static string ValidateEmail(string email)
+        {
+            if (email is not null)
+            {
+                bool isValid = email.Contains("@") && email.Contains(".");
+
+                if (isValid)
+                {
+                    return email;
+                }
+            }
+
+            return "Não Válido";
+        }
+    }
+}
